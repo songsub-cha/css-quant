5238341e603e76e7acf352d999e9f921a9c73c88
차송섭 <songsub@chasongseob-ui-Macmini.local>
2026-07-22 19:42:44 +0900

    feat: implement plan for issue #11

--- apps/api/alembic/versions/fe4de3edb1d3_initial_wiring.py
+++ apps/api/alembic/versions/fe4de3edb1d3_initial_wiring.py
@@ -10,13 +10,13 @@ Create Date: 2026-07-21 22:33:19.023103
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
 # revision identifiers, used by Alembic.
 revision: str = "fe4de3edb1d3"
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
